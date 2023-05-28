c35623e376d6fa40c080d84dc070860daa69163c
Vivek <viveksah59@gmail.com>
2023-05-28 14:29:35 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import logo from '../images/logo.svg'
+import {pagelinks,socialLinks} from '../data';
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="backroads" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          {/**<!-- left this comment on purpose -->*/}
+          <ul className="nav-links" id="nav-links">
+            {pagelinks.map((links)=>{
+               return (
+               <li key={links.id}>
+                  <a href={links.href} className="nav-link">
+                    {links.text}
+                  </a>
+                </li>
+                );
+            })}
+            </ul>
+
+          <ul className="nav-icons">
+            {socialLinks.map((links)=>{
+                const {id, href, icon} = links;
+                return (
+                  <li key={id}>
+                    <a
+                      href={href}
+                      target="_blank"
+                      className="nav-icon"
+                    >
+                      <i className={icon}></i>
+                    </a>
+                  </li>
+                );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Tours.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Title from "./Title";
+import { tour } from "../data";
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="featured" subTitle="tours" />
+
+      <div className="section-center featured-center">
+        {tour.map((tours) => {
+          const { id, img, date, title, info, place, days, price } = tours;
+          return (
+            <article className="tour-card" key={id}>
+              <div className="tour-img-container">
+                <img src={img} className="tour-img" alt="" />
+                <p className="tour-date">{date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{title}</h4>
+                </div>
+                <p>{info}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {place}
+                  </p>
+                  <p>{days} days</p>
+                  <p>from ${price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
